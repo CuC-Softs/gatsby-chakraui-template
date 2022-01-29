01c03b6e15a2485470293e315cb2d1cab2a46ace
eduardor2m <deveduardomelo@gmail.com>
2022-01-29 05:59:16 -0300

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,20 @@ module.exports = {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
+      '@chakra-ui/gatsby-plugin',
+      {
+        resolve: `gatsby-plugin-typescript`,
+        options: {
+          isTSX: true, // defaults to false
+          jsxPragma: `jsx`, // defaults to "React"
+          allExtensions: true, // defaults to false
+        },
+      },
+      {
+        resolve: `gatsby-plugin-styled-components`,
+        options: {
+          // Add any options here
+        },
+      }
     ]
 }
